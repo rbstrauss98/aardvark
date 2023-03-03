72b9853957977dce92dc365e2392325cda7dc6de
Dallin <bowiedallin@gmail.com>
2023-03-02 20:18:18 -0700

    functionality for editing an existing bug

--- aardvark-frontend/src/components/Modal/EditBugModal.js
+++ aardvark-frontend/src/components/Modal/EditBugModal.js
@@ -5,36 +5,50 @@ import { Modal } from 'react-responsive-modal';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 
-function CreateBugModal() {
+function EditBugModal(props) {
   const apiURL = process.env.REACT_APP_LOCAL_API_URL //use for developing
   const [open, setOpen] = useState(false);
 
   const onOpenModal = () => setOpen(true);
   const onCloseModal = () => setOpen(false);
 
+  function openModal(){
+	onOpenModal()
+	setTitle(props.title);
+	setDescription(props.description);
+  }
+
 
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
-
   
   // Used on form submission
   const handleSubmit = ((event) => {
-    event.preventDefault();
+	event.preventDefault();
+
+	if (props.title == title && props.description == description){
+		//fixme: is this the behavior we want when they don't change the title or description??
+		onCloseModal();
+		return;
+	}
+
+	props.updateBug(title, description);
+
     console.log("Title: " + title);
     console.log("Description: " + description);
 
 
     // Create request object
     const requestOptions = {
-      method: 'POST',
+      method: 'PUT',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ title: title, description: description})
     };
 
 
     // Make api call and log response
-    fetch(`${apiURL}/api/bugs/`, requestOptions)
-      .then(response => console.log(response.json()))
+    fetch(`${apiURL}/api/bugs/${props.bugID}`, requestOptions)
+      .then(response => console.log(response.text()))
 
     // Close modal (there might be a better way to do this)
     onCloseModal();
@@ -48,14 +62,14 @@ function CreateBugModal() {
 
   return (
     <div>
-      <Button onClick={onOpenModal} variant="outline-success">Create Bug 🐜</Button>{' '}
+      <Button onClick={openModal} variant="outline-success">Edit Bug 🐜</Button>{' '}
       <div className="mb-3"></div> {/* Used for spacing */}
       {/* <button onClick={onOpenModal}>Create Bug</button> */}
       <Modal open={open} onClose={onCloseModal} center>
         <Form onSubmit={handleSubmit}>
           <Form.Group className="mb-3" controlId="formBasicTitle">
               <Form.Label>Enter Title of Bug</Form.Label>
-              <Form.Control type="text" placeholder="Bug" onChange={(event) => setTitle(event.target.value)} />
+              <Form.Control type="text" value={title} onChange={(event) => setTitle(event.target.value)} />
               <Form.Text className="text-muted">
                 Use a descriptive name
               </Form.Text>
@@ -63,7 +77,7 @@ function CreateBugModal() {
 
           <Form.Group className="mb-3" controlId="formBasicDescription">
               <Form.Label>Description</Form.Label>
-              <Form.Control type="text" placeholder="Description" onChange={(event) => setDescription(event.target.value)}/>
+              <Form.Control type="text" value={description} onChange={(event) => setDescription(event.target.value)}/>
           </Form.Group>
 
           <Button variant="primary" type="submit" value="Submit">
@@ -75,4 +89,7 @@ function CreateBugModal() {
   );
 };
 
-export default CreateBugModal
+export default EditBugModal
+
+
+//fixme: modal flashes when closing
--- aardvark-frontend/src/pages/bug.js
+++ aardvark-frontend/src/pages/bug.js
@@ -2,17 +2,27 @@ import React, { useState, useEffect } from "react";
 import { Container, Row, Col, Card, Image } from "react-bootstrap";
 import { useParams } from 'react-router-dom'
 import AddCommentModal from '../components/Modal/AddCommentModal';
+import EditBugModal from '../components/Modal/EditBugModal';
 
 const BugPage = () => {
   const apiURL = process.env.REACT_APP_LOCAL_API_URL //use for developing
   const { id } = useParams();
   const [bug, setBug] = useState({});
 
+  function handleTitleDescriptionChange(newTitle, newDescription) {
+    setBug({
+      ...bug,
+      title: newTitle, 
+      description: newDescription
+    });
+  }
+
   useEffect(() => {
     const fetchBug = async () => {
       const response = await fetch(`${apiURL}/api/bugs/${id}`);
       const bug = await response.json();
       setBug(bug);
+      console.log(bug)
     };
     fetchBug();
   }, [id]);
@@ -24,6 +34,7 @@ const BugPage = () => {
           <h1>{bug.title}</h1>
           <p>{bug.description}</p>
           <img src={bug.image} alt={bug.title} />
+          <EditBugModal title={bug.title} description={bug.description} bugID={bug.id} updateBug={handleTitleDescriptionChange}></EditBugModal>
           <AddCommentModal bugID={bug.id}></AddCommentModal>
         </Col>
         {bug.comments && bug.comments.map(comment => (
